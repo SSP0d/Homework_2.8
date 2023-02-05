43d0a56e9b497b9fdaa93e11329940e91ef1b234
Serhii Pidkopai <spodkopay@gmail.com>
2023-02-06 00:41:01 +0200

    models

---
+++ consumer.py
@@ -0,0 +1,13 @@
+from time import sleep
+
+import pika
+
+
+def main():
+    credentials = pika.PlainCredentials('guest', 'guest')
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost', port=5672, credentials=credentials))
+    channel = connection.channel()
+
+    def callback(ch, method, properties, body):
+        pass
--- models.py
+++ models.py
@@ -1,2 +1,15 @@
-from mongoengine import EmbeddedDocument, Document
-from mongoengine.fields import BooleanField, DateTimeField, EmbeddedDocumentField, ListField, StringField
+from mongoengine import Document, CASCADE
+from mongoengine.fields import ListField, StringField, DateField, ReferenceField
+
+
+class Author(Document):
+    fullname = StringField(required=True, unique=True)
+    born_date = DateField()
+    born_location = StringField()
+    description = StringField()
+
+
+class Quote(Document):
+    tags = ListField(StringField(max_length=20))
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    quote = StringField()
