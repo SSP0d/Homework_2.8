91438b774a7e168b67cdfdc2a302723b856cbbd9
Serhii Pidkopai <spodkopay@gmail.com>
2023-02-09 16:57:25 +0200

    finder fix

--- finder.py
+++ finder.py
@@ -10,8 +10,8 @@ cache = RedisLRU(client)
 
 def inpup_check(command) -> dict:
     args = command.split(':')
-    key = args[0]
-    value = args[-1].strip().split(',')
+    key = args[0] if len(command.split(':')) > 1 else command.strip()
+    value = args[-1].strip().split(',') if len(command.split(':')) > 1 else ''
     return {key: value}
 
 
@@ -25,17 +25,17 @@ def find_quotes_to_name(name):
                 or quote.author.fullname.lower().startswith(name.lower()):
             author_name = quote.author.fullname
             all_quotes.append(quote.quote)
-    return f'All quotes by {author_name}: {", ".join(el for el in all_quotes)}'
+    return f'All quotes by "{author_name}": {", ".join(el for el in all_quotes)}'
 
 
 @cache
-def find_quote_to_tag(tag_to_search):
+def find_quote_to_tag(tag_to_search) -> list:
     all_quotes = []
     quotes = Quotes.objects()
     for quote in quotes:
         for tag in quote.tags:
             if tag.startswith(tag_to_search):
-                all_quotes.append(f'Quote by {quote.author.fullname} with tag "{tag}tag:life": {quote.quote}')
+                all_quotes.append(f'Quote by "{quote.author.fullname}" with tag "{tag}": {quote.quote}')
     return all_quotes
 
 
@@ -50,16 +50,16 @@ def main():
             commands = inpup_check(command)
 
             if 'name' in commands:
-                for name in command['name']:
+                for name in commands['name']:
                     print(find_quotes_to_name(name))
 
             if 'tag' in commands:
                 for value in commands['tag']:
-                    print(find_quote_to_tag(value))
+                    print(f'{",".join([el for el in find_quote_to_tag(value)])}')
 
             if 'tags' in commands:
                 for value in commands['tags']:
-                    print(find_quote_to_tag(value))
+                    print(f'{",".join([el for el in find_quote_to_tag(value)])}')
 
 
 if __name__ == '__main__':
